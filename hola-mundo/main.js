6c7a24485022abfa5ac6550d0cf79d13829a4fd1
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-10-24 22:46:39 -0600

    Se agregaron comentarios Se paso al español

--- hola-mundo/main.js
+++ hola-mundo/main.js
@@ -1,6 +1,10 @@
+// ES Module
 import reactDOM from "https://esm.sh/react-dom@18.2.0/client";
 
+// Elemento raíz de la aplicación
 const appDomElement = document.getElementById("app");
 
+// Creación del elemento raíz
 const root = reactDOM.createRoot(appDomElement);
-root.render("Hello World!");
+// Renderizado del elemento raíz
+root.render("¡Hola mundo!");
