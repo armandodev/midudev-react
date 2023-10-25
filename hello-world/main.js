efc4831178f1dda92ac7d15261712c9353008f19
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-10-24 22:39:12 -0600

    Hola mundo

---
+++ hello-world/main.js
@@ -0,0 +1,6 @@
+import reactDOM from "https://esm.sh/react-dom@18.2.0/client";
+
+const appDomElement = document.getElementById("app");
+
+const root = reactDOM.createRoot(appDomElement);
+root.render("Hello World!");
