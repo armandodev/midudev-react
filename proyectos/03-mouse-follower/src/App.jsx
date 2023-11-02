b40a8ea1ffee8d23412ee8848beb02fc03e2862b
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-11-02 17:00:37 -0600

    Add mouse pointer follow functionality to App component

--- proyectos/03-mouse-follower/src/App.jsx
+++ proyectos/03-mouse-follower/src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from 'react'
+
 function App () {
-  return <h3>Proyecto 3</h3>
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({ x: 0, y: 0 })
+    }
+  }, [enabled])
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgb(0 0 0 / 50%)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  )
 }
 
 export default App
