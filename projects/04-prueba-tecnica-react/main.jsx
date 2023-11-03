e3aca7c989223a6a748530969d7d623f8559dc53
ArmandoDev <jorge.armando.c.cardenas@gmail.com>
2023-11-03 14:43:31 -0600

    Delete unnecessary files and code

---
+++ projects/04-prueba-tecnica-react/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import './style.css'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <h1>Hola mundo</h1>
+)
--- projects/04-react-prueba-tecnica/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App () {
-  return (
-    <>
-      <h3>Prueba técnica</h3>
-    </>
-  )
-}
-
-export default App
